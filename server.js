448dea1a65ee881f792aa289099a040745f97513
rancongjie <95633676@qq.com>
2015-12-08 11:28:52 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+/**
+ * Created by rancongjie on 15/12/8.
+ */
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
+
+function send404(res) {
+  res.writeHead(404, {'Content-Type': 'text-plain'});
+  res.write('error:404');
+  res.end();
+}
+function sendFiles(res, filePath, fileContents) {
+  res.writeHead(200, {'Content-Type': mime.lookup(path.basename(filePath))});
+  res.end(fileContents);
+}
+
+//缓存服务
+function serverCache(res, cache, absPath) {
+  if (cache[absPath]) {
+    sendFiles(res, absPath, cache[absPath]);
+  } else {
+    fs.exists(absPath, function (exists) {
+      if (exists) {
+        fs.readFile(absPath, function (err, data) {
+          if (err) {
+            send404(res);
+          } else {
+            cache[absPath] = data;
+            sendFiles(res, absPath, data);
+          }
+        });
+      } else {
+        send404(res);
+      }
+    });
+  }
+}
+
+var server = http.createServer(function (req, res) {
+  var filePath;
+  if (req.url === '/') {
+    filePath = 'public/index.html';
+  } else {
+    filePath = 'public' + req.url;
+
+  }
+  var absPath = './' + filePath;
+  serverCache(res, cache, absPath);
+
+});
+
+server.listen(3000, function () {
+  console.log('服务启动');
+});
